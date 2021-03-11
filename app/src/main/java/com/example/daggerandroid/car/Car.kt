86696c9c12f5c594608af057b2c51dd5611d5902
Hitesh Chopra <hiteshchopra11@gmail.com>
2021-03-12 00:50:09 +0530

    Field Injection any class

--- app/src/main/java/com/example/daggerandroid/car/Car.kt
+++ app/src/main/java/com/example/daggerandroid/car/Car.kt
@@ -1,17 +1,29 @@
 package com.example.daggerandroid.car
 
 import android.util.Log
+import com.example.daggerandroid.Remote
 import javax.inject.Inject
 
 
 // @Inject lets Dagger know how to create instances of these objects
+// Removed Constructor Injection for Wheels class and used Field Injection for the same below
+class Car @Inject constructor(private val engine: Engine) {
 
-/*If we directly try to inject Engine interface inside our
- *Car class, we will get an exception as Dagger is not able
- *create an Engine object. We need to use module for the same.
- */
-class Car @Inject constructor(private val wheels: Wheels, private val engine: Engine) {
+    /** Field Injection to provide Wheels Class object to Car
+     *  class using Field, instead of passing the same through constructor
+     */
+
+    @Inject
+    lateinit var wheels: Wheels
+
+    @Inject
+    fun provideCarToRemote(remote: Remote) {
+        Log.d("Provide car", "Provided car to remote successfully!")
+        remote.provideCar(this)
+    }
+
     fun driveCar() {
+        Log.d("Wheels", "Wheels using Field Injection")
         engine.start()
         Log.d("Car", "Driving the Car....")
     }
--- app/src/main/java/com/example/daggerandroid/di/module/WheelsModule.kt
+++ app/src/main/java/com/example/daggerandroid/di/module/WheelsModule.kt
@@ -1,4 +1,38 @@
 package com.example.daggerandroid.di.module
 
+import android.util.Log
+import com.example.daggerandroid.car.Wheels
+import com.example.daggerandroid.car.wheels.Rims
+import com.example.daggerandroid.car.wheels.Tyres
+import dagger.Module
+import dagger.Provides
+
+@Module
 class WheelsModule {
+
+    // Dagger uses @Provides to create Rims and Tyres object and finally
+    // these instances are injected to Wheels
+
+    @Provides
+    fun providesRims(): Rims {
+        Log.d("Rims", "Rims")
+        return Rims()
+    }
+
+    @Provides
+    fun providesTyres(): Tyres {
+        Log.d("Tyres", "Tyres")
+        return Tyres()
+    }
+
+
+    //Dagger now knows how to create rims and tyres object as we have used @Provides and
+    //defined methods to provide the
+    @Provides
+    fun providesWheels(rims: Rims, tyres: Tyres): Wheels {
+
+        // We manually create Wheels class object and return it
+        Log.d("Wheels", "Wheels Module with tyres and rims")
+        return Wheels(rims, tyres)
+    }
 }
