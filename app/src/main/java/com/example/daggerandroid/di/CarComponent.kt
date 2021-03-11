c4bd3e27b829d20255d67b55113e5347525d4bc1
Hitesh Chopra <hiteshchopra11@gmail.com>
2021-03-11 15:16:41 +0530

    Field Injection

--- app/src/main/java/com/example/daggerandroid/di/CarComponent.kt
+++ app/src/main/java/com/example/daggerandroid/di/CarComponent.kt
@@ -1,12 +1,19 @@
 package com.example.daggerandroid.di
 
 import com.example.daggerandroid.Car
+import com.example.daggerandroid.MainActivity
 import dagger.Component
 
 // @Component makes Dagger create a graph of dependencies
 @Component
 interface CarComponent {
+
     // The return type  of functions inside the component interface is
     // what can be provided from the container
     fun getCar():Car
+
+    // Field Injection using inject function as we need to inject the MainActivity
+    // inside CarComponent to use the fields(variables) of MainActivity
+    fun inject(mainActivity: MainActivity)
+
 }
