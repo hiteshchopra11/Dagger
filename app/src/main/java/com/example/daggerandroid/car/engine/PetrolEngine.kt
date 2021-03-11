6fabe374f3612cae470a17e0277a084114b3d62c
Hitesh Chopra <hiteshchopra11@gmail.com>
2021-03-12 01:41:24 +0530

    Inject Runtime Values

--- app/src/main/java/com/example/daggerandroid/car/engine/PetrolEngine.kt
+++ app/src/main/java/com/example/daggerandroid/car/engine/PetrolEngine.kt
@@ -5,8 +5,22 @@ import com.example.daggerandroid.car.Engine
 import javax.inject.Inject
 
 // @Inject lets Dagger know how to create instance of PetrolEngine
-class PetrolEngine @Inject constructor() : Engine {
+
+/**Dagger can create an instance of the PetrolEngine only if the class
+ * has an empty constructor and @Inject annotation. Integer "powerCapacity" does not
+ * have an @Inject annotation, so error is thrown while building
+ * if we use the below given lines of code without making any
+ * changes in the PetrolEngineModule class */
+
+class PetrolEngine @Inject constructor(private val powerCapacity: Int) : Engine {
+
     override fun start() {
-        Log.d("Petrol engine", "Petrol Engine Started")
+        Log.d("Petrol engine", "Petrol Engine Started PowerCapacity is $powerCapacity")
     }
 }
+
+
+/** Therefore to use the PetrolEngine class with a Field, we need to manually create instance
+ * of PetrolEngine class with relevant fields inside the providesPetrolEngine() method in the
+ * PetrolEngineModule instead of passing the instance of PetrolEngine through constructor.
+ */
--- app/src/main/java/com/example/daggerandroid/di/module/PetrolEngineModule.kt
+++ app/src/main/java/com/example/daggerandroid/di/module/PetrolEngineModule.kt
@@ -2,9 +2,9 @@ package com.example.daggerandroid.di.module
 
 import com.example.daggerandroid.car.Engine
 import com.example.daggerandroid.car.engine.PetrolEngine
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import javax.inject.Inject
 
 /* If we directly try to inject Engine interface inside our
  * Car class, we will get an exception as Dagger is not able
@@ -14,9 +14,34 @@ import dagger.Provides
  */
 
 @Module
-abstract class PetrolEngineModule {
+class PetrolEngineModule @Inject constructor(private val powerCapacity: Int) {
 
-    //We can use @Binds in place of @Provides if class is abstract
-    @Binds
-    abstract fun providesPetrolEngine(petrolEngine: PetrolEngine): Engine
+    /*We return the instance of PetrolEngine passed through the
+     *constructor only if petrolEngine has no parameters to be passed*/
+
+    /*  @Provides
+     *  fun providesPetrolEngine(petrolEngine: PetrolEngine): Engine{
+     *      return petrolEngine
+     *  }
+     */
+
+    /*As our PetrolEngine class has parameters now, we can't directly pass
+    * the instance of PetrolEngine through the constructor, instead we need
+    * to manually create the instance with the appropriate parameters. The
+    * below code achieves this but uses "hard-coded" value.*/
+
+    /* @Provides
+     *  fun providesPetrolEngine(): Engine {
+     *     return PetrolEngine(100)
+     *  }
+     */
+
+    /* To pass the value "dynamically" we can use powerCapacity value from the
+     * PetrolEngineModule constructor. Instructions on how to pass the values
+     */
+
+    @Provides
+    fun providesPetrolEngine(): Engine {
+        return PetrolEngine(powerCapacity)
+    }
 }
